11e619487d460e1fdc6777fcb60e7806b547c6ea
elubow <eric@lubow.org>
2011-10-13 12:04:49 -0700

    Take warning. (don't change the pid)

--- sranalytics_admin.php
+++ sranalytics_admin.php
@@ -12,16 +12,24 @@
     if ($message) {
         print '<div id="message" class="updated below-h2">'. $message . '</div>';
     }
+
+    $sranalytics_pid = get_option('sranalytics_pid');
 ?>
 <div class="overview" style="margin-left:10px;">
     <h2><?php _e('SimpleReach Analytics', 'sranalytics'); ?></h2>
 
-    <div id="sranalytics_controls">
+    <div id="sranalytics_controls" style="margin-left:5px;">
         <form name="sranalytics_form" method="post" action="<?php print str_replace( '%7E', '~', $_SERVER['REQUEST_URI']); ?>">
             <input type="hidden" name="sranalytics_submitted" value="1" />
-            <label for="sranalytics_submitted" style="margin-left:5px;"><?php _e('Enter your Publisher ID (PID): ', 'sranalytics'); ?></label>
-            <input type="text" name="sranalytics_pid" value="<?php print get_option('sranalytics_pid') ;?>" style="width:200px;" />
+            <label for="sranalytics_submitted"><?php _e('Enter your Publisher ID (PID): ', 'sranalytics'); ?></label>
+            <input type="text" name="sranalytics_pid" value="<?php print $sranalytics_pid; ?>" style="width:200px;" />
             <input type="submit" name="Submit" value="<?php _e('Save', 'sranalytics'); ?>" />
+            <?php if (!empty($sranalytics_pid) && isset($sranalytics_pid)) { ?>
+                <br />
+                <span style="color:red;font-size:10px;">
+                    * Do not change this unless you are absolutely sure you know what you are doing!
+                </span>
+            <?php } ?>
         </form>
     </div>
 </div>
