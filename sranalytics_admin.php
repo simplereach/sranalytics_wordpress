73b08835139de250246a1862180e1f8ececeb6be
elubow <eric@lubow.org>
2011-10-13 11:50:00 -0700

    Some styling and verbage cleanup

--- sranalytics_admin.php
+++ sranalytics_admin.php
@@ -6,21 +6,21 @@
             $message = _("ERROR:  You must enter a value for pid!", 'sranalytics');
         } else {
             update_option('sranalytics_pid', $sranalytics_pid);
-            $message = 'pid saved!';
+            $message = 'You have updated your publisher ID.';
         }
     }
     if ($message) {
         print '<div id="message" class="updated below-h2">'. $message . '</div>';
     }
 ?>
-<div class="overview">
+<div class="overview" style="margin-left:10px;">
     <h2><?php _e('SimpleReach Analytics', 'sranalytics'); ?></h2>
 
     <div id="sranalytics_controls">
         <form name="sranalytics_form" method="post" action="<?php print str_replace( '%7E', '~', $_SERVER['REQUEST_URI']); ?>">
             <input type="hidden" name="sranalytics_submitted" value="1" />
-            <label for="sranalytics_submitted"><?php _e('Enter your Publisher ID (PID): ', 'sranalytics'); ?></label>
-            <input type="text" name="sranalytics_pid" value="<?php print get_option('sranalytics_pid') ;?>" />
+            <label for="sranalytics_submitted" style="margin-left:5px;"><?php _e('Enter your Publisher ID (PID): ', 'sranalytics'); ?></label>
+            <input type="text" name="sranalytics_pid" value="<?php print get_option('sranalytics_pid') ;?>" style="width:200px;" />
             <input type="submit" name="Submit" value="<?php _e('Save', 'sranalytics'); ?>" />
         </form>
     </div>
